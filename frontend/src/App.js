be91fe5727aed202c229a1a46d0772b9a622edf4
gberger <gberger@paloaltonetworks.com>
2021-08-30 11:47:21 +0300

    fix logout buttom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
                 </i>
               ) : (
                 <i className="fa fa-sign-out" aria-hidden="true">
-                  <span className="userName"> Logout ({username})</span>
+                  <span className="userName"> Hi, {username}</span>
                 </i>
               )}
             </Link>
